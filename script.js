25750ddcff0ee0bf22f1af1ba81e09d2332924a5
GBusta <104409829+GBusta@users.noreply.github.com>
2022-06-20 18:25:31 -0500

    added another blank line (line 16) to make code more ogranized.

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ var randomUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', '
 var randomNum = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 var randomSpecialChar = ["!","@","#","$","%","^","&","*","(",")","{","}","[","]","=","<",">","/",",","."];
+
 charLength = prompt("How many characters would you want your password to be between 8-128?");
     if (charLength < 8 || charLength > 128) {
     return alert("Please enter a valid length.");
